e1453034481cb6ae5f4cfbafa28946120e99d402
student <student@safebear.co.uk>
2019-11-04 11:07:38 +0000

    Initial commit

---
+++ src/test/java/co/uk/safebear/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package co.uk.safebear;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunCucumberTest {
+}
