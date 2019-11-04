6cf208ca3d32922d03abf6e43f59b17bdcc87ff4
student <student@safebear.co.uk>
2019-11-04 11:32:56 +0000

    Added Hello comment

--- src/test/java/co/uk/safebear/RunCucumberTest.java
+++ src/test/java/co/uk/safebear/RunCucumberTest.java
@@ -4,6 +4,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
+//Hello
+
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty"})
 public class RunCucumberTest {
