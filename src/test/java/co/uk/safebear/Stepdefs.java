f580782a49ba2eb9606e6cb79e87741eb764ffa3
student <student@safebear.co.uk>
2019-11-04 14:41:26 +0000

    Created Cucumber file and Step Definition

--- src/test/java/co/uk/safebear/Stepdefs.java
+++ src/test/java/co/uk/safebear/Stepdefs.java
@@ -8,4 +8,23 @@ import static org.junit.Assert.*;
 
 public class Stepdefs {
 
+    @Given("I am logged out")
+    public void i_am_logged_out() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I enter username {string} and password {string}")
+    public void i_enter_username_and_password(String username, String password) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the user is informed that the login is successful")
+    public void the_user_is_informed_that_the_login_is_successful() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
 }
