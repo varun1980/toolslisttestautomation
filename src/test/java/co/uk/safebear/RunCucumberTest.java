45109acf39285b2416347edbc50d2121d0b85503
student <student@safebear.co.uk>
2019-11-04 13:57:38 +0000

    Adding cucumber and webdrivermanager dependencies

--- src/test/java/co/uk/safebear/RunCucumberTest.java
+++ src/test/java/co/uk/safebear/RunCucumberTest.java
@@ -4,9 +4,13 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-//Hello
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(
+        plugin = {"json:target/cucumber-repor/cucumber.json"},
+        tags ="not @to-do"
+        //glue ="path to Stepdefs if not in the same folder",
+        //features ="path to features if not in co.uk.safebear folder"
+)
 public class RunCucumberTest {
 }
