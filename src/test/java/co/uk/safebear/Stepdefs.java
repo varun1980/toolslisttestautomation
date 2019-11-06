c4cac05f04a3e00bc9617bcfc712cd7dd1b1e3ff
student <student@safebear.co.uk>
2019-11-06 16:03:13 +0000

    Updated StepDefs

--- src/test/java/co/uk/safebear/Stepdefs.java
+++ src/test/java/co/uk/safebear/Stepdefs.java
@@ -10,6 +10,7 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.WebDriver;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
 
 public class Stepdefs {
@@ -47,14 +48,15 @@ public class Stepdefs {
 
     @When("I enter username {string} and password {string}")
     public void i_enter_username_and_password(String username, String password) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+       loginPage.enterUsername(username);
+       loginPage.enterPassword(password);
+       loginPage.clickSubmitButton();
     }
 
     @Then("the user is informed that the login is successful")
     public void the_user_is_informed_that_the_login_is_successful() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        assertThat("Login failed or the Login Successful message didn't appear", toolsPage.checkForSuccessfulLoginMessage(),containsString ("Login Successful"));
+
     }
 
 
