8a5307e01e6f39c4861c7f41c69f4f39b97d7bba
student <student@safebear.co.uk>
2019-11-07 10:02:05 +0000

    Successful Login Test no bdd

---
+++ src/test/java/co/uk/safebear/LoginTest.java
@@ -0,0 +1,46 @@
+package co.uk.safebear;
+
+import co.uk.safebear.pages.LoginPage;
+import co.uk.safebear.pages.ToolsPage;
+import co.uk.safebear.utils.Driver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import sun.rmi.runtime.Log;
+
+public class LoginTest {
+
+    WebDriver browser;
+    LoginPage loginPage;
+    ToolsPage toolspage;
+
+    @Before
+    public void setup(){
+        browser = Driver.getDriver();
+        loginPage = new LoginPage(browser);
+        toolspage = new ToolsPage(browser);
+        browser.navigate().to(Driver.getUrl());
+
+    }
+
+    @After
+    public void teardown()
+    {
+        browser.quit();
+
+    }
+
+    @Test
+    public void successfulLoginTest(){
+
+        Assert.assertEquals("Login Page",loginPage.getPageTitle());
+
+        loginPage.enterUsername("tester");
+        loginPage.enterPassword("letmein");
+        loginPage.clickSubmitButton();
+
+        Assert.assertEquals("Login Successful",toolspage.checkForSuccessfulLoginMessage());
+    }
+}
